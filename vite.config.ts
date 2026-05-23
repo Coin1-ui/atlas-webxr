3e993e3e50d1c035290de380cb736b8b348d8ebe
Coin1-ui <aryan.barua57@gmail.com>
2026-05-23 21:30:26 +0530

    Update Vite config to set base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/atlas-webxr/',
+})
+
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { VitePWA } from "vite-plugin-pwa";
 import { customModelsApiPlugin } from "./plugins/custom-models-api";
